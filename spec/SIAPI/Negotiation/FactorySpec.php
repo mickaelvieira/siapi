45ecbbeec6a6f4fbb46fe2f4e32125dc81c8647a
Mickael Vieira <mickael.vieira@intudigital.co.uk>
2014-09-11 18:24:00 +0100

    Added Factory Dependency Injection Specs

--- spec/SIAPI/Negotiation/FactorySpec.php
+++ spec/SIAPI/Negotiation/FactorySpec.php
@@ -5,6 +5,10 @@ namespace spec\SIAPI\Negotiation;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * Class FactorySpec
+ * @package spec\SIAPI\Negotiation
+ */
 class FactorySpec extends ObjectBehavior
 {
     function it_is_initializable()
@@ -28,5 +32,29 @@ class FactorySpec extends ObjectBehavior
     {
         $this::build('media', array())->shouldHaveType('\SIAPI\Negotiation\Negotiator\Media');
         $this::build('media', array())->shouldHaveType('\SIAPI\Negotiation\Negotiator');
+    }
+
+    function it_should_inject_the_dependencies_to_media_negotiator()
+    {
+        $this::build('media', array())->getStrategy()->shouldHaveType('\SIAPI\Negotiation\Strategy\Media');
+        $this::build('media', array())->getCollection()->shouldHaveType(
+            '\SIAPI\Negotiation\Header\Accept\Collection\Media'
+        );
+    }
+
+    function it_should_inject_the_dependencies_to_charset_negotiator()
+    {
+        $this::build('charset', array())->getStrategy()->shouldHaveType('\SIAPI\Negotiation\Strategy\Charset');
+        $this::build('charset', array())->getCollection()->shouldHaveType(
+            '\SIAPI\Negotiation\Header\Accept\Collection\Charset'
+        );
+    }
+
+    function it_should_inject_the_dependencies_to_language_negotiator()
+    {
+        $this::build('language', array())->getStrategy()->shouldHaveType('\SIAPI\Negotiation\Strategy\Language');
+        $this::build('language', array())->getCollection()->shouldHaveType(
+            '\SIAPI\Negotiation\Header\Accept\Collection\Language'
+        );
     }
 }
