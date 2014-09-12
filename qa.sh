aa7027c176bd1c5e4ef9467a3ea97c1e367c4e25
Mickael Vieira <mickael.vieira@intudigital.co.uk>
2014-09-12 17:44:44 +0100

    Updated QA script

--- qa.sh
+++ qa.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-PHPCS="$(which phpcs)"
-PHPMD="$(which phpmd)"
+NEW_LINE="\r\n"
+PHPSPEC_URL="http://www.phpspec.net/"
+PHPCS_URL="http://pear.php.net/package/PHP_CodeSniffer/"
+PHPMD_URL="http://phpmd.org/"
 
-echo "Running PhpSpec"
+if ! type "phpcs" > /dev/null; then
+    echo "PHP Code Sniffer does not seem to be installed. See. ${PHPCS_URL}"
+    exit 1
+fi
+if ! type "phpmd" > /dev/null; then
+    echo "PHP Mess Detector does not seem to be installed. See. ${PHPMD_URL}"
+    exit 1
+fi
+
+
+echo "Running PhpSpec. See. ${PHPSPEC_URL}\n"
 ./bin/phpspec run
+echo ${NEW_LINE}
 
-echo "Running PHPCS"
-${PHPCS} --standard=PSR2 src/
+echo "Running PHP Code Sniffer. See. ${PHPCS_URL}\n"
+phpcs --standard=PSR2 src/
+echo ${NEW_LINE}
 
-echo "Running PHPMD"
-${PHPMD} src/ text codesize, controversial, design, naming, unusedcode
+echo "Running Mess Detector. See. ${PHPMD_URL}\n"
+phpmd src/ text codesize, controversial, design, naming, unusedcode
+echo ${NEW_LINE}
