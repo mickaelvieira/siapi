3de5b395fb006c65eedd40d9d30026646daaf089
Mickael Vieira <contact@mickael-vieira.com>
2014-10-06 16:56:39 +0100

    Added Document and Image entities

---
+++ src/SIAPI/Entity/Document.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickael
+ * Date: 03/10/14
+ * Time: 23:27
+ */
+
+namespace SIAPI\Entity;
+
+
+class Document {
+
+} 
---
+++ src/SIAPI/Entity/Image.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickael
+ * Date: 03/10/14
+ * Time: 22:59
+ */
+
+namespace SIAPI\Entity;
+
+
+class Image {
+
+} 
