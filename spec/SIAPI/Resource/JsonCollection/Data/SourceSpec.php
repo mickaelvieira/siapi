fad386ee712a17184fee98e3ad65ba30a9fb4256
Mickael Vieira <contact@mickael-vieira.com>
2014-10-20 14:27:15 +0100

    Added JsonCollection data spec

---
+++ spec/SIAPI/Resource/JsonCollection/Data/SourceSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace spec\SIAPI\Resource\JsonCollection\Data;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class SourceSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('SIAPI\Resource\JsonCollection\Data\Source');
+    }
+
+    function it_should_have_the_right_name_value()
+    {
+        $this->getName()->shouldBeEqualTo('source');
+    }
+
+    function it_should_have_the_right_prompt_value()
+    {
+        $this->getPrompt()->shouldBeEqualTo('Source');
+    }
+
+    function it_should_not_be_required()
+    {
+        $this->shouldNotBeRequired();
+    }
+}
