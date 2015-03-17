7332a332400fc434ed28b8370bbb0cee894afa28
Mickael Vieira <contact@mickael-vieira.com>
2015-03-17 18:22:06 +0000

    Result class is now an interface

--- src/SIAPI/Component/Search/Result.php
+++ src/SIAPI/Component/Search/Result.php
@@ -2,33 +2,10 @@
 
 namespace SIAPI\Component\Search;
 
-use JsonSerializable;
-use SIAPI\Component\ArrayConvertible;
-
-class Result implements ArrayConvertible, JsonSerializable
+interface Result
 {
     /**
-     * @return array
+     * @return mixed
      */
-    public function toArray()
-    {
-        $data = get_object_vars($this);
-        array_walk(
-            $data,
-            function (&$value) {
-                if (is_object($value) && $value instanceof ArrayConvertible) {
-                    $value = $value->toArray();
-                }
-            }
-        );
-        return $data;
-    }
-
-    /**
-     * @return array
-     */
-    public function jsonSerialize()
-    {
-        return get_object_vars($this);
-    }
-} 
+    public function getId();
+}
